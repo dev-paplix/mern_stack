2678adcba0dc750204e013c41a38e10ad83c629c
Nurul Izzah Binti Zainudin <nurulizzah@smg.my>
2025-05-29 11:37:56 +0800

    allowance page

--- mern-app/frontend/src/pages/TrainingPages/Izzah.jsx
+++ mern-app/frontend/src/pages/TrainingPages/Izzah.jsx
@@ -1,10 +1,50 @@
-
+import { employee_allowance } from "../../assets/icons"
 
 const Izzah = () => {
   return (
-    <div>
-      <h1>Izzah's Training Page</h1>
+    
+    <div class="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl ">
+      
+  <div>
+    <img class="size-48 shadow-xl rounded-md" alt="" src={employee_allowance} />
+  </div>
+  <div class="flex items-center md:items-start">
+    <span class="text-2xl font-medium">Employee Allowance</span>
+  </div>
+  <div>
     </div>
+    <div class="h-14 bg-linear-to-r from-cyan-500 to-blue-500">
+      Request
+    </div>
+    <table class="table-auto">
+  <thead>
+    <tr>
+      <th>Item</th>
+      <th>Min</th>
+      <th>Max</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Item 1</td>
+      <td>RM 100</td>
+      <td>RM 200</td>
+    </tr>
+     <tr>
+      <td>Item 2</td>
+      <td>RM 150</td>
+      <td>RM 250</td>
+    </tr>
+    <tr>
+      <td>Item 3</td>
+      <td>RM 200</td>
+      <td>RM 250</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+  
+  
   )
 }
 
