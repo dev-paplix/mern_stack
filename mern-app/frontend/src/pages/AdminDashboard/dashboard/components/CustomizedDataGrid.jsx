bb5250d52bf2881fc094f0ff245121249ceb82e2
devops.arman <paplix.dev@gmail.com>
2025-05-29 15:20:34 +0800

    basic table

--- mern-app/frontend/src/pages/AdminDashboard/dashboard/components/CustomizedDataGrid.jsx
+++ mern-app/frontend/src/pages/AdminDashboard/dashboard/components/CustomizedDataGrid.jsx
@@ -8,7 +8,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import getEmployees from '../../API/items-get';
+import getEmployees from '../../API/getEmployees';
 
 
 
