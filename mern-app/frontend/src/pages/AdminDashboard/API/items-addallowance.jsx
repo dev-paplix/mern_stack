2c217fdad5e0a6e8ee5ef3391d0217347abcef5e
Nurul Izzah Binti Zainudin <nurulizzah@smg.my>
2025-05-30 15:18:49 +0800

    upd

--- mern-app/frontend/src/pages/AdminDashboard/API/items-addallowance.jsx
+++ mern-app/frontend/src/pages/AdminDashboard/API/items-addallowance.jsx
@@ -1,6 +1,6 @@
 const BACKEND_URI = "http://localhost:5000/api";
 
-export async function addAllowance({ user, employeesAllowanceAmount, employeesAllowanceReason, employeesExtra, employeesforLead}) {
+export async function addAllowance({ userId, employeesAllowanceAmount, employeesAllowanceReason, employeesExtra}) {
   const token = localStorage.getItem('token');
   const response = await fetch(`${BACKEND_URI}/employee-allowance`, {
     method: 'POST',
@@ -8,7 +8,7 @@ export async function addAllowance({ user, employeesAllowanceAmount, employeesAl
       'Content-Type': 'application/json',
       Authorization: token ? `Bearer ${token}` : undefined,
     },
-    body: JSON.stringify({ user, employeesAllowanceAmount, employeesAllowanceReason, employeesExtra, employeesforLead}),
+    body: JSON.stringify({ userId, employeesAllowanceAmount, employeesAllowanceReason, employeesExtra}),
   });
   if (!response.ok) {
     const error = await response.json();
